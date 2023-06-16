1f444f27278ae4d7798d5a1be56fe1a40ff3b421
Rickybolo99 <riccardo.bologna99@gmail.com>
2023-06-16 12:34:58 +0200

    getAllFilms funzionante
    funziona per ora

--- Project/Back-end/server.js
+++ Project/Back-end/server.js
@@ -11,51 +11,17 @@ const fs = require('fs');
 
 const utenti = require('./MOCK_DATA.json')
 
+const pgPromise = require('pg-promise');
+const { get } = require('https');
 
-/*
-fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
- 
-    console.log("JSON file has been saved.");
-});
-*/
+const connStr = 'postgresql://postgres@localhost:5432/dvdrental'; 
+
+const pgp = pgPromise({}); // empty pgPromise instance
+const psql = pgp(connStr); // get connection to your db instance
+
+
 
 client.connect();
-/*
-client.query('select * from film', (err,res2)=>{
-    console.log(res2.rows)
-})
-
-
-const getFilms = (req, res) =>{
-    client.query('select * from film', (err,result)=>{
-        res.send("ok")
-    })}
-
-
-
-
-const l = (req,res) => {client.query('select * from film')}
-console.log(l.rows)
-
-async function getAllFilm(){
-    const rows = await client.query('select * from film');
-    console.log(rows)
-    return rows;
-}
-let ok = (req,res)=>{
-    client.query('select * from film', (err,res2)=>{
-        res.json(res2.rows)
-    })
-}
-
-
-*/
-//console.log(JSON.stringify(client.query('select * from film')))
-
 
 
 // handling CORS
@@ -87,63 +53,10 @@ const UserType = new GraphQLObjectType({
     })
 })
 
-const variabile =   [{
-    film_id: 89,
-    title: 'Borrowers Bedazzled',
-    description: 'A Brilliant Epistle of a Teacher And a Sumo Wrestler who must Defeat a Man in An Abandoned Fun House',
-    release_year: 2006,
-    language_id: 1,
-    rental_duration: 7,
-    rental_rate: '0.99',
-    length: 63,
-    replacement_cost: '22.99',
-    rating: 'G',
-    special_features: [ 'Commentaries', 'Deleted Scenes', 'Behind the Scenes' ],
-    fulltext: "'abandon':20 'bedazzl':2 'borrow':1 'brilliant':4 'defeat':15 'epistl':5 'fun':21 'hous':22 'man':17 'must':14 'sumo':11 'teacher':8 'wrestler':12"
-  },
-  {
-    film_id: 89,
-    title: 'Borrowers Bedazzled',
-    description: 'A Brilliant Epistle of a Teacher And a Sumo Wrestler who must Defeat a Man in An Abandoned Fun House',
-    release_year: 2006,
-    language_id: 1,
-    rental_duration: 7,
-    rental_rate: '0.99',
-    length: 63,
-    replacement_cost: '22.99',
-    rating: 'G',
-    special_features: [ 'Commentaries', 'Deleted Scenes', 'Behind the Scenes' ],
-    fulltext: "'abandon':20 'bedazzl':2 'borrow':1 'brilliant':4 'defeat':15 'epistl':5 'fun':21 'hous':22 'man':17 'must':14 'sumo':11 'teacher':8 'wrestler':12"
-  }]
-//console.log(variabile)
 
-
-
-
-async function getAllFilm(){
-    try{
-        const rows = await client.query('select * from film');
-        const l = rows.rows
-        return JSON.stringify(l)
-    }
-    catch(error){
-        return null
-    }
-}
-const righe = getAllFilm().then(function(result){return result})
-
-
-
-
-
-
-
-
-
-
-
-
-
+const filmList = ()=>(
+    client.query("select * from film").then((valore)=>(valore.rows)).catch((error)=>(console.log(error)))
+)
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
@@ -151,9 +64,8 @@ const RootQuery = new GraphQLObjectType({
         getAllFilm: {
             type: new GraphQLList(FilmType),
             resolve(parent, args){
-            //console.log(getAllFilm().then(function(result){return result}))
-            return async ()=>{await getAllFilm()}
-
+            
+            return filmList()
             }
         }
     }
@@ -168,7 +80,7 @@ const Mutation = new GraphQLObjectType({
                 title:{type:GraphQLString},
             },
             resolve(parent, args) {
-                return async ()=>{await getAllFilm()}
+                
             }
         }
     }
