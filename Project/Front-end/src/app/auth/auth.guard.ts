dfc595b184039fd165c0de68edb9ef23e79bfbef
HL99s <65026011+HL99s@users.noreply.github.com>
2023-07-06 00:05:53 +0200

    Fix: - Fixed searchFilmByTitle in filmsComponent - Migrated page redirect to Angular Router

--- Project/Front-end/src/app/auth/auth.guard.ts
+++ Project/Front-end/src/app/auth/auth.guard.ts
@@ -1,10 +1,14 @@
-
+import { Router } from '@angular/router';
+import {inject} from '@angular/core';
 export const AuthGuard = () => {
+
+  const router = inject(Router);
 
   if (localStorage.getItem("TOKEN")) {
     return true;
   }
   // Redirect to the login page
-  window.location.href = "/login";
-  return false;
+  //window.location.href = "/login";
+
+  return router.parseUrl('/login');
 };
